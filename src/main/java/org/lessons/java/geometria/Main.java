64db6245cb550d314b90919b1a554849b23ac816
RosaCielo <ashyzan@gmail.com>
2024-04-22 20:36:50 +0200

    ADD: user fields

--- src/main/java/org/lessons/java/geometria/Main.java
+++ src/main/java/org/lessons/java/geometria/Main.java
@@ -1,10 +1,46 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
+
 public class Main {
+	// creare il metodo main(), nel quale chiedete all’utente di inserire, 
+		// da console, i valori di base e di altezza con cui istanziare un nuovo Rettangolo. 
+		// Dopo averlo istanziato, stampate a video il perimetro e l’area.
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Benvenuto nell'area geometria.");
+		
+		/* Rettangolo rettangolo1 = new Rettangolo();
+		rettangolo1.base = 20;
+		rettangolo1.altezza = 40;
+		rettangolo1.calcolaArea();
+		rettangolo1.calcolaPerimetro();
+		*/
+		
+		// metodo Scanner permette di prendere un input dalla tastiera
+				Scanner input = new Scanner(System.in);
+				// apre il field per inserimento dati utente
+				System.out.println("Ciao, inserisci il valore dell'area del rettangolo");
+			
+				
+				// attribuisce alla variabile nome l'input inserito dall'utente
+				int baseRettangoloUtente = input.nextInt();
+				System.out.println("La base del tuo rettangolo è " + baseRettangoloUtente);
+				int altezzaRettangoloUtente = input.nextInt();
+				System.out.println("L'altezza del tuo rettangolo è " + baseRettangoloUtente);
+				
+				Rettangolo rettangoloUtente = new Rettangolo();
+				rettangoloUtente.base = baseRettangoloUtente;
+				rettangoloUtente.altezza = altezzaRettangoloUtente;
+				rettangoloUtente.calcolaArea();
+				rettangoloUtente.calcolaPerimetro();
+				
+				
+				
+	}
 
 	
-	// creare il metodo main(), nel quale chiedete all’utente di inserire, 
-	// da console, i valori di base e di altezza con cui istanziare un nuovo Rettangolo. 
-	// Dopo averlo istanziato, stampate a video il perimetro e l’area.
+	
 }
 
--- src/main/java/org/lessons/java/geometria/Rettangolo.java
+++ src/main/java/org/lessons/java/geometria/Rettangolo.java
@@ -7,19 +7,21 @@ public class Rettangolo {
 	int altezza;
 	// Aggiungere un opportuno costruttore con parametri.
 	// Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+	
 	void calcolaArea() {
 		
 		int area = base * altezza;
-		
-		System.out.println(area);
+		System.out.println("calcolo l'area del retangolo...");
+		System.out.println("l'area è.. " + area);
 		
 	}
 	
 	void calcolaPerimetro() {
 		
 		int perimetro = (base * 2) + (altezza * 2) ;
+		System.out.println("calcolo il perimetro del retangolo...");
 		
-		System.out.println(perimetro);
+		System.out.println("il perimetro è.. " + perimetro);
 	}
 }
  
